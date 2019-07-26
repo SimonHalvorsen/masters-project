52e9a9d5b1bad3c7bba3a126f5c436ce27de7149
Simon Halvorsen <halvorsen.simon@gmail.com>
2019-07-26 16:13:42 +0200

    started writing about the cli implementation

--- alarm-service/src/main/java/com/mastersproject/alarmservice/kafkaProducer/AlarmProducerWithCallback.java
+++ alarm-service/src/main/java/com/mastersproject/alarmservice/kafkaProducer/AlarmProducerWithCallback.java
@@ -36,13 +36,14 @@ public class AlarmProducerWithCallback {
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
 
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
-//    the number of unacknowledged requests the client will send before blocking
-//    properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
-//    high throughput producer at the expense of a bit of latency and CPU usage
-//    properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
-//    properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
-//    properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+        // the number of unacknowledged requests the client will send before blocking
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        // high throughput producer at the expense of a bit of latency and CPU usage
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "10");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
 
         this.producer = new KafkaProducer<>(properties);
     }
